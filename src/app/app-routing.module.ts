4aa8f60eaec8d0b80dc6940a64b0e6cd0e04728e
Wilber Donayres Olayunca <wilberd29@gmail.com>
2021-01-07 19:10:25 -0500

    fin seccion 3 diseño

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PrincipalComponent } from './pages/principal.component';
+
+const routes: Routes = [
+{ path: '', component: PrincipalComponent,
+  children: [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'progress', component: ProgressComponent },
+    { path: 'grafica1', component: Grafica1Component },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+},
+
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+
+
+{ path: '**', component: NopagefoundComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes ),
+    CommonModule
+  ],
+  exports: [ RouterModule]
+})
+export class AppRoutingModule { }
